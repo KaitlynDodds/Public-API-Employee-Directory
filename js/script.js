1c8007015b8448c39392e176ab38b81fcf1f96bc
KaitlynDodds <kadodds.dev@kadodds.com>
2018-05-16 13:36:58 -0600

    address and state formatting applied

--- js/script.js
+++ js/script.js
@@ -94,19 +94,78 @@ function buildLightBox(employeeData) {
 
 	$('.lightbox .phone').text(`${employeeData.cell}`);
 
-	$('.lightbox .address').text(`${employeeData.location.street}, ${getStateAbrv(employeeData.location.state)}, ${employeeData.location.postcode}`);
+	$('.lightbox .address').text(getFormattedAddress(employeeData));
 
-	$('.lightbox .dob').text(`Birthday: ${formatDate(new Date(employeeData.dob))}`);
+	$('.lightbox .dob').text(`Birthday: ${getFormattedDate(new Date(employeeData.dob))}`);
 
 	$('.lightbox').css('display', 'block');
 }
 
-function getStateAbrv(state) {
-	return 'WY';
+function getFormattedAddress(employeeData) {
+	return `${employeeData.location.street}, ${getStateAbbr(employeeData.location.state)}, ${employeeData.location.postcode}`;
 }
 
-function formatDate(date) {
-	
+function getStateAbbr(state) {
+	var states = {
+        'arizona': 'AZ',
+        'alabama': 'AL',
+        'alaska': 'AK',
+        'arizona': 'AZ',
+        'arkansas': 'AR',
+        'california': 'CA',
+        'colorado': 'CO',
+        'connecticut': 'CT',
+        'delaware': 'DE',
+        'florida': 'FL',
+        'georgia': 'GA',
+        'hawaii': 'HI',
+        'idaho': 'ID',
+        'illinois': 'IL',
+        'indiana': 'IN',
+        'iowa': 'IA',
+        'kansas': 'KS',
+        'kentucky': 'KY',
+        'kentucky': 'KY',
+        'louisiana': 'LA',
+        'maine': 'ME',
+        'maryland': 'MD',
+        'massachusetts': 'MA',
+        'michigan': 'MI',
+        'minnesota': 'MN',
+        'mississippi': 'MS',
+        'missouri': 'MO',
+        'montana': 'MT',
+        'nebraska': 'NE',
+        'nevada': 'NV',
+        'new hampshire': 'NH',
+        'new jersey': 'NJ',
+        'new mexico': 'NM',
+        'new york': 'NY',
+        'north carolina': 'NC',
+        'north dakota': 'ND',
+        'ohio': 'OH',
+        'oklahoma': 'OK',
+        'oregon': 'OR',
+        'pennsylvania': 'PA',
+        'rhode island': 'RI',
+        'south carolina': 'SC',
+        'south dakota': 'SD',
+        'tennessee': 'TN',
+        'texas': 'TX',
+        'utah': 'UT',
+        'vermont': 'VT',
+        'virginia': 'VA',
+        'washington': 'WA',
+        'west virginia': 'WV',
+        'wisconsin': 'WI',
+        'wyoming': 'WY',
+    };
+
+    return states[state];
+}
+
+function getFormattedDate(date) {
+
 	function formatWithTwoDigits(val) {
 		return (val.toString().length > 1 ? `${val}` : `0${val}`);
 	}
